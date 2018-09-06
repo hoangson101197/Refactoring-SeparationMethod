05ff3ecc279c8efb281c6dc1ee9660ca1061e145
= <=>
2018-09-06 09:29:47 +0700

    separation method

--- src/Cylinder.java
+++ src/Cylinder.java
@@ -1,8 +1,19 @@
 public class Cylinder {
-    public static double getVolume(int radius, int height){
-        double baseArea = Math.PI * radius * radius;
-        double perimeter = 2 * Math.PI  * radius;
+    public double getVolume(int radius, int height){
+        double baseArea = getBaseArea(radius);
+        double perimeter = getPerimeter(radius);
         double volume = perimeter * height + 2 * baseArea;
         return volume;
+    }
+
+    public Cylinder() {
+    }
+
+    private double getPerimeter(int radius) {
+        return 2 * Math.PI * radius;
+    }
+
+    private double getBaseArea(int radius) {
+        return Math.PI * radius * radius;
     }
 }
